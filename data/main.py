45e21bf0ba8b73445cf1eca696e8c88911bac8aa
Your Name <you@example.com dorothy.corley@stonybrook.edy>
2023-05-13 02:35:23 +0000

    made update to main.py

--- data/main.py
+++ data/main.py
@@ -1,26 +1,23 @@
 from fastapi import FastAPI
 import pandas as pd
-
+import uvicorn
 
 df = pd.read_csv('./data/diagnoses2019.csv')
 
-
 app = FastAPI()
 
-
-@app.get('/')
+app.get('/')
 def home():
-    return 'this is a API service for MN ICD code details'
+    return {'this is an API service for MN code details'}
 
-
-@app.get('/preview')
+app.get('/preview')
 async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
 
 
-@app.get('/icd/{value}') 
+app.get('/icd/{value}') 
 async def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
@@ -30,11 +27,12 @@ async def icdcode(value):
         return filtered.to_json(orient="records")
 
 
-@app.get('/icd/{value}/sex/{value2}') 
+app.get('/icd/{value}/sex/{value2}') 
 async def icdcode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
     else:
-        return filtered2.to_json(orient="records")   
+        return filtered2.to_json(orient="records")
+
