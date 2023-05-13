9e59d43e4e2c6bc690599fe91c6a09fb300f2f16
Your Name <you@example.com dorothy.corley@stonybrook.edy>
2023-05-13 01:16:57 +0000

    added vercel and requirements

--- data/main.py
+++ data/main.py
@@ -1,38 +1,40 @@
-from flask import Flask, request 
-import pandas as pd 
+from fastapi import FastAPI
+import pandas as pd
+
 
 df = pd.read_csv('./data/diagnoses2019.csv')
-app = Flask(__name__)
 
-app.route('/', methods=["GET"])
+
+app = FastAPI()
+
+
+@app.get('/')
 def home():
     return 'this is a API service for MN ICD code details'
 
-app.route('/preview', methods=["GET"])
-def preview():
+
+@app.get('/preview')
+async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
 
-app.route('/icd/<value>', methods=['GET'])
-def icdcode(value):
+
+@app.get('/icd/{value}') 
+async def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
-    else: 
+    else:
         return filtered.to_json(orient="records")
 
-app.route('/icd/<value>/sex/<value2>')
-def icdcode2(value, value2):
+
+@app.get('/icd/{value}/sex/{value2}') 
+async def icdcode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
-    else: 
-        return filtered2.to_json(orient="records")    
-    
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+    else:
+        return filtered2.to_json(orient="records")   
